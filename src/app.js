2919d9f78f28fa472792cf5689e2e3c1ac450e1a
Nicky Kalu <nkalu@kemri-wellcome.org>
2020-08-13 16:23:01 +0300

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -9,15 +9,57 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('./src/json/accounts.json', {encoding: 'UTF8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('./src/json/users.json', {encoding: 'UTF8'});
+const users = JSON.parse(userData);
+
 app.get('/',(req, res) => {
   res.render(
     'index', 
     {
-      title: 'Index'
+      title: 'Account Summary',
+      accounts: accounts,
+    }
+  );
+});
+app.get('/savings',(req, res) => {
+  res.render(
+    'account', 
+    {
+      title: 'Savings Account Summary',
+      account: accounts.savings,
+    }
+  );
+});
+app.get('/credit',(req, res) => {
+  res.render(
+    'account', 
+    {
+      title: 'Credit Account Summary',
+      account: accounts.credit,
+    }
+  );
+});
+app.get('/checking',(req, res) => {
+  res.render(
+    'account', 
+    {
+      title: 'Checking Account Summary',
+      account: accounts.checking,
     }
   );
 });
 
+app.get('/profile',(req, res) => {
+  res.render(
+    'profile', 
+    {
+      user: users[0]
+    }
+  );
+});
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
 })
