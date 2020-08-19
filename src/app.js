b36409984d99f975eca47b95d5536a1411cbe42c
Nicky Kalu <nkalu@kemri-wellcome.org>
2020-08-19 14:32:06 +0300

    Module 4

--- src/app.js
+++ src/app.js
@@ -4,17 +4,14 @@ const express = require('express');
 
 const app = express();
 
+const { accounts, users, writeJSON} = require('./data');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 
-const accountData = fs.readFileSync('./src/json/accounts.json', {encoding: 'UTF8'});
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('./src/json/users.json', {encoding: 'UTF8'});
-const users = JSON.parse(userData);
 
 app.get('/',(req, res) => {
   res.render(
@@ -32,11 +29,7 @@ app.post('/transfer', (req, res) => {
   const { from, to, amount } = req.body;
   accounts[from].balance = parseInt(accounts[from].balance) - parseInt(amount);
   accounts[to].balance = parseInt(accounts[to].balance) + parseInt(amount);
-  let accountsJSON = JSON.stringify(accounts, null, 4);
-
-  fs.writeFileSync(
-    path.join(__dirname, 'json/accounts.json'), 
-    accountsJSON, 'UTF8');
+  writeJSON();
   res.render(
     'transfer',
     {
@@ -57,11 +50,7 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
   accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount);
   accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-  let accountsJSON = JSON.stringify(accounts, null, 4);
-
-  fs.writeFileSync(
-    path.join(__dirname, 'json/accounts.json'), 
-    accountsJSON, 'UTF8');
+  writeJSON();
   res.render(
     'payment',
     {
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const accountData = fs.readFileSync('./src/json/accounts.json', {encoding: 'UTF8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('./src/json/users.json', {encoding: 'UTF8'});
+const users = JSON.parse(userData);
+
+writeJSON = () => {
+  let accountsJSON = JSON.stringify(accounts, null, 4);
+
+  fs.writeFileSync(
+    path.join(__dirname, 'json/accounts.json'), 
+    accountsJSON, 'UTF8');
+}
+
+module.exports = {
+  accounts,
+  users,
+  writeJSON
+};
